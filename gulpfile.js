c5a273becc91361a748629681fd3d68a3bd2dfae
GustavoFernandes <GustavoFernandes@users.noreply.github.com>
2018-03-05 19:07:38 -0800

    Move source files into src. Create build tasks in gulpfile. Add styles. Rename deploy dir to docs

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,25 @@ var gulp = require('gulp');
 var ts = require('gulp-typescript');
 var Server = require('karma').Server;
 
-gulp.task('default', function () {
+gulp.task('default', ['build']);
+
+gulp.task('build', ['build-html'], function () {
   return gulp.src('src/**/*.ts')
       .pipe(ts({
         noImplicitAny: true,
         outFile: 'main.js'
       }))
-      .pipe(gulp.dest('dist'));
+      .pipe(gulp.dest('docs'));
+});
+
+gulp.task('build-html', ['build-css'], function () {
+  return gulp.src('src/index.html')
+      .pipe(gulp.dest('docs'));
+});
+
+gulp.task('build-css', function () {
+  return gulp.src('src/styles.css')
+      .pipe(gulp.dest('docs'));
 });
 
 gulp.task('test', function (done) {
